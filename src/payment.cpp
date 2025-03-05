1c85e4a7a3880ffd9f7cbfc6c175dd47490aefd7
insanansharyrasul <insanansryrasul21@gmail.com>
2025-03-05 09:39:53 +0700

    feat - added feature to save to txt file

--- src/payment.cpp
+++ src/payment.cpp
@@ -1,26 +1,50 @@
 #include "payment.h"
 
+#include <iomanip>
+#include <fstream>
 #include <iostream>
 #include <string>
 using namespace std;
+
+const string DATA_PATH = "../data/payment.txt";
 
 void payTuitionFee() {
     string studentName;
     double amount;
 
-    cout << "Enter student name: ";
+    cout << "Masukkan nama siswa: ";
     cin >> studentName;
-    cout << "Enter amount: ";
+    cout << "Masukkan jumlah: ";
     cin >> amount;
 
-    cout << "Payment of $" << amount << " for " << studentName << " has been processed." << endl;
+    ofstream outFile;
+    outFile.open(DATA_PATH, ios::app);
+    if (outFile.is_open()) {
+        cout << "Pembayaran dari Rp." << amount << " untuk " << studentName << " telah diproses." << endl;
+        outFile << studentName << " " << fixed << setprecision(2) << amount << endl;
+        outFile.close();
+    } else {
+        cout << "Gagal menyimpan data." << endl;
+    }
 }
 
 void checkPaymentStatus() {
     string studentName;
 
-    cout << "Enter student name: ";
+    cout << "Masukkan nama siswa: ";
     cin >> studentName;
 
-    cout << "Payment status for " << studentName << ": Paid" << endl;
+    cout << "Pembayaran untuk" << studentName << ": Dibayar" << endl;
+    ifstream inFile(DATA_PATH);
+    string line;
+
+    if (inFile.is_open()) {
+        cout << "=== STATUS PEMBAYARAN ===" << endl;
+        while (getline(inFile, line)) {
+            cout << fixed << setprecision(2) << line << endl;
+        }
+        inFile.close();
+    } else {
+        cout << "Gagal membaca data" << endl;
+    }
 }
