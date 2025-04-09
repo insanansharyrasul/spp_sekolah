af1ed9e17681450102cedc171e877ec3c1125c51
insanansharyrasul <insanansryrasul21@gmail.com>
2025-04-09 18:43:54 +0700

    fix - changed to pragma once for the remaining file

--- include/account/siswa.hpp
+++ include/account/siswa.hpp
@@ -1,10 +1,7 @@
-#ifndef SISWA_H
-#define SISWA_H
+#pragma once
 #include "menu.hpp"
 #include <iostream>
 
 int verify_siswa(std::string&);
 void show_menu_siswa(std::string);
 VerificationStatus handle_siswa_choice(int, int);
-
-#endif
--- include/constants/file_path.hpp
+++ include/constants/file_path.hpp
@@ -1,10 +1,7 @@
-#ifndef FILE_PATH_HPP
-#define FILE_PATH_HPP
+#pragma once
 #include <string>
 #include <unistd.h>
 #include <limits.h>
 
 const std::string SISWA_DATA_PATH = "../data/siswa_data.txt";
 const std::string SPP_DATA_PATH = "../data/spp_data.txt";
-
-#endif
